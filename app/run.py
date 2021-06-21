01cb7e70485f2dcfb7ffe7cafb9536928c831fdd
EriRika <egintautas@yahoo.de>
2021-06-21 17:23:31 +0000

    first commit

---
+++ app/run.py
@@ -0,0 +1,154 @@
+import json
+import plotly
+import pandas as pd
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+from flask import Flask
+from flask import render_template, request, jsonify
+from plotly.graph_objs import Bar
+from sklearn.externals import joblib
+from sqlalchemy import create_engine
+
+
+app = Flask(__name__)
+
+def tokenize(text):
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+# load data
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('DisasterResponse', engine)
+
+# load model
+model = joblib.load("../models/classifier.pkl")
+
+
+# indexwebpage displays cool visuals and receives user input text for model
+@app.route('/')
+@app.route('/index')
+def index():
+    
+    # extract data needed for visuals
+    # TODO: Below is an example - modify to extract data for your own visuals
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
+    
+    correlation = df.iloc[:,4:40].corr()
+    melt = pd.melt(df, id_vars=['id', 'message', 'genre'], value_vars=df.iloc[:,4:40].columns)
+    grouped = melt.groupby(['genre','variable'], as_index = False)['value'].sum().sort_values(by='value', ascending= False)
+    grouped_new = grouped[~grouped.variable.str.contains('related')].sort_values(by='value', ascending= False)
+    
+    # create visuals
+    # TODO: Below is an example - modify to create your own visuals
+    graphs = [      
+        #Barplot ba genre
+         {
+            'data': [
+                {
+                    'x': grouped_new[grouped_new.genre == 'direct'].variable,
+                    'y': grouped_new[grouped_new.genre == 'direct'].value,
+                    'type': 'bar',
+                    'color': 'rrgba(255,153,51,0.6)',
+                    'name': 'direct'
+                    
+                },
+                {
+                    'x': grouped_new[grouped_new.genre == 'news'].variable,
+                    'y': grouped_new[grouped_new.genre == 'news'].value,
+                    'color': 'rgba(55,128,191,0.6)',
+                    'type': 'bar',
+                    'name': 'news'
+                
+                },
+                {
+                    'x': grouped_new[grouped_new.genre == 'social'].variable,
+                    'y': grouped_new[grouped_new.genre == 'social'].value,
+                    'color': 'rgba(55,128,191,0.6)',
+                    'type': 'bar',
+                    'name': 'social'
+                
+                }
+            ],
+
+            'layout': {
+                'barmode': 'stack',
+                'title': 'Messages by cluster and genre',
+                'yaxis': {
+                    'title': "Number of messages"
+                },
+                'xaxis': {
+                    'title': "clusters"
+                },
+            }
+        },
+       
+        #heatmap correlation by cluster
+         {
+            'data': [
+                {
+                    'z': correlation.values,
+                    'x': correlation.columns.tolist(),
+                    'y': correlation.columns.tolist(),
+                    'type': 'heatmap',
+                    'colorscale': 'YlOrRd'
+                }
+            ],
+
+            'layout': {
+                'title': 'Correlation of clusters',
+                'yaxis': {
+                    'title': "clusters"
+                },
+                'xaxis': {
+                    'title': "clusters"
+                },
+                #'width': 800,
+                'height': 800
+            }
+        }
+
+    ]
+    
+    # encode plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    # render web page with plotly graphs
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+
+
+# web page that handles user query and displays model results
+@app.route('/go')
+def go():
+    # save user input in query
+    query = request.args.get('query', '') 
+
+    # use model to predict classification for query
+    classification_labels = model.predict([query])[0]
+    classification_results = dict(zip(df.columns[4:], classification_labels))
+
+    # This will render the go.html Please see that file. 
+    return render_template(
+        'go.html',
+        query=query,
+        classification_result=classification_results
+    )
+
+
+def main():
+    app.run(host='0.0.0.0', port=3001, debug=True)
+    
+
+
+if __name__ == '__main__':
+    main()
---
+++ data/process_data.py
@@ -0,0 +1,92 @@
+import sys
+import pandas as pd
+import numpy as np
+import sqlalchemy as db
+from sqlalchemy import create_engine
+
+def load_data(messages_filepath, categories_filepath):
+    """
+    Load CSVs and merge them into one dataframe
+    Merging is based on commin id
+    """
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, on = 'id', how='left')
+    return df
+
+
+def clean_data(df):
+    """
+    Clean categorie columns, by extracting binary values from string and by renaming columns
+    Drop duplicates after cleaning
+    """
+    categories = df.categories.str.split(";", expand=True)
+    row = categories.iloc[0,:]
+
+    #extract a list of new column names for categories.
+    category_colnames = [x.split("-")[0] for x in row]
+
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str.split("-",expand= True)[1]
+
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    # drop the original categories column from `df`
+    df.drop(columns = 'categories', inplace = True)
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+    df.drop_duplicates(inplace = True)
+42
+    df = pd.concat([df, categories], axis=1)
+43
+    df.drop_duplicates(inplace = True)
+44
+    
+45
+    #column related contained class 2, but it was only 2, if all other columns were 0, which was also true for related = 0, hence I overwrite it
+    
+    #column related contained class 2, but it was only 2, if all other columns were 0, which was also true for related = 0, hence I overwrite it
+    df.related.replace(2,0, inplace = True)
+
+    return df
+
+
+def save_data(df, database_filename):
+    engine = create_engine('sqlite:///data/' + database_filename + '.db')
+    df.to_sql(database_filename, engine, index=False, if_exists='replace')
+    pass  
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
